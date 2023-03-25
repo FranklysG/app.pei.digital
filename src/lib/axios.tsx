78ab239cb264715da642ebf221a3c6d419e0bd8c
FranklysG <tunele095@gmail.com>
2023-03-25 00:40:13 -0300

    return xmlhttprequest

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -4,6 +4,7 @@ const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
     'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
     Accept: 'application/json',
   },
   withCredentials: true,
